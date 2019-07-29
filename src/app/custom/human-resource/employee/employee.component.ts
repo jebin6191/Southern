a6540d70b7ea8041fb5590ab67b043ee24c31600
jebin6191 <jebin6191@gmail.com>
2019-07-29 09:27:11 +0530

    changes

--- src/app/custom/human-resource/employee/employee.component.ts
+++ src/app/custom/human-resource/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -10,9 +10,38 @@ export class EmployeeComponent implements OnInit {
 
   EmployeeForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.EmployeeForm = this.formBuilder.group({
+      FirstName : ['', Validators.required],
+      LastName : ['', Validators.required],
+      Gender : ['', Validators.required],
+      FatherName : ['', Validators.required],
+      DateOfBirth : ['', Validators.required],
+      MaritalStatus : ['', Validators.required],
+      ContactNo : ['', Validators.required],
+      Email : ['', Validators.required],
+      Photo : ['', Validators.required],
+      MedicalExam : ['', Validators.required],
+      CompanyName : ['', Validators.required],
+      DesignationName : ['', Validators.required],
+      ReportTo : ['', Validators.required],
+      CurrentAddress : ['', Validators.required],
+      State : ['', Validators.required],
+      City : ['', Validators.required],
+      PermanentAddress : ['', Validators.required],
+      AdhaarNo : ['', Validators.required],
+      AlternateContactNo : ['', Validators.required],
+      MotherName : ['', Validators.required],
+      BloodGroup : ['', Validators.required],
+      DOJ : ['', Validators.required],
+      ReferenceBy : ['', Validators.required],
+      ReferenceContact1 : ['', Validators.required],
+      ReferenceContact2 : ['', Validators.required],
+      PreviousCompany : ['', Validators.required],
+      JobType : ['', Validators.required],
+    })
   }
 
 }
